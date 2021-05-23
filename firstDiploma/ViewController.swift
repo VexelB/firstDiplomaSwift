6ffffa858fbfd48861808d94de33e08d1f28d3ad
VexelB <vexelb@gmail.com>
2021-05-23 16:38:33 +0300

    Initial Commit

---
+++ firstDiploma/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firstDiploma
+//
+//  Created by Евгений Войтин on 23.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
